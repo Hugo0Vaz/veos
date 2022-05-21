22ba6594415136b21ddaab44dd2e667eed6fd624
Hugo0Vaz <hugomartinsvaz@gmail.com>
2022-05-21 19:27:20 -0300

    project cleanup

--- veos-xmonad/xmonad_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Syu xmonad-contrib \
-                 xmonad \
-                 xmobar \
-                 cabal-install \
-                 xdg-user-dirs \
-                 xdg-utils \
-                 wget \
